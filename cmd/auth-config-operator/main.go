290e5b88b51766c0b00b58e5c312439ce9090a18
EvB <evb@redhat.com>
2018-10-23 21:40:49 -0400

    wip(sync): add basic control loop

--- cmd/auth-config-operator/main.go
+++ cmd/auth-config-operator/main.go
@@ -10,14 +10,14 @@ import (
 	k8sutil "github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 
-	"github.com/sirupsen/logrus"
+	"github.com/golang/glog"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
 func printVersion() {
-	logrus.Infof("Go Version: %s", runtime.Version())
-	logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
-	logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
+	glog.Infof("Go Version: %s", runtime.Version())
+	glog.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
+	glog.Infof("operator-sdk Version: %v", sdkVersion.Version)
 }
 
 func main() {
@@ -29,11 +29,15 @@ func main() {
 	kind := "ClusterAuthentication"
 	namespace, err := k8sutil.GetWatchNamespace()
 	if err != nil {
-		logrus.Fatalf("failed to get watch namespace: %v", err)
+		glog.Fatalf("failed to get watch namespace: %v", err)
 	}
 	resyncPeriod := 5
-	logrus.Infof("Watching %s, %s, %s, %d", resource, kind, namespace, resyncPeriod)
+	glog.Infof("Watching %s, %s, %s, %d", resource, kind, namespace, resyncPeriod)
 	sdk.Watch(resource, kind, namespace, time.Duration(resyncPeriod)*time.Second)
-	sdk.Handle(stub.NewHandler())
+	handler, err := stub.NewHandler()
+	if err != nil {
+		glog.Fatalf("failed to initialize client: %v", err)
+	}
+	sdk.Handle(handler)
 	sdk.Run(context.TODO())
 }
--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -1,27 +1,81 @@
 package stub
 
 import (
+	"bytes"
 	"context"
-	"fmt"
+	"encoding/json"
+	"errors"
+	"reflect"
 
 	"github.com/openshift/auth-config-operator/pkg/apis/auth/v1alpha1"
 
+	"github.com/golang/glog"
+	kubeapiserverconfigclientv1alpha1 "github.com/openshift/cluster-kube-apiserver-operator/pkg/generated/clientset/versioned/typed/kubeapiserver/v1alpha1"
+	"github.com/operator-framework/operator-sdk/pkg/k8sclient"
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
-func NewHandler() sdk.Handler {
-	return &Handler{}
+const (
+	ClusterAuthResourceNameDefault         = "instance"
+	KubeApiserverOperatorConfigNameDefault = "instance"
+)
+
+func NewHandler() (sdk.Handler, error) {
+	kclient, err := kubeapiserverconfigclientv1alpha1.NewForConfig(k8sclient.GetKubeConfig())
+	if err != nil {
+		return nil, err
+	}
+	return &Handler{
+		ClusterAuthResourceName:         ClusterAuthResourceNameDefault,
+		KubeApiserverOperatorConfigName: KubeApiserverOperatorConfigNameDefault,
+		KubeapiserverConfigClient:       kclient,
+	}, nil
 }
 
 type Handler struct {
-	// Fill me
+	ClusterAuthResourceName         string
+	KubeApiserverOperatorConfigName string
+
+	KubeapiserverConfigClient kubeapiserverconfigclientv1alpha1.KubeapiserverV1alpha1Interface
 }
 
 func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 	switch o := event.Object.(type) {
 	case *v1alpha1.ClusterAuthentication:
-		fmt.Printf("Got ClusterAuthentication resource: %#v\n", o)
-		return nil
+		glog.Infof("syncing clusterauth: %#v", o)
+		if err := h.sync(o); err != nil {
+			glog.Errorf("error syncing clusterauth: %v", err)
+			return err
+		}
 	}
 	return nil
+}
+
+func (h *Handler) sync(ca *v1alpha1.ClusterAuthentication) error {
+	if ca == nil {
+		return errors.New("unknown ClusterAuthentication resource: nil")
+	}
+	if ca.GetName() != h.ClusterAuthResourceName {
+		glog.Warningf("unknown ClusterAuthentication resource: name='%s'", ca.GetName())
+		return nil
+	}
+	kconfig, err := h.KubeapiserverConfigClient.KubeApiserverOperatorConfigs().
+		Get(h.KubeApiserverOperatorConfigName, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	userConfig := v1alpha1.ClusterAuthenticationSpec{}
+	json.NewDecoder(bytes.NewBuffer(kconfig.Spec.UserConfig.Raw)).Decode(&userConfig)
+	if reflect.DeepEqual(ca.Spec, userConfig) {
+		return nil
+	}
+	raw, err := json.Marshal(ca.Spec)
+	if err != nil {
+		return err
+	}
+	kconfig.Spec.UserConfig = runtime.RawExtension{Raw: raw}
+	_, err = h.KubeapiserverConfigClient.KubeApiserverOperatorConfigs().Update(kconfig)
+	return err
 }
